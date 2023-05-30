9f8699f748ddb1f67d8c3b844131662d967bfe7e
EC2 Default User <ec2-user@ip-172-31-23-21.ap-northeast-1.compute.internal>
2023-05-30 12:42:47 +0000

    [Finish]4-9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,17 @@ class ApplicationController < ActionController::Base
   # deviseの機能（ユーザ登録、ログイン認証など）が使われる前にオレンジのメソッドが実行される
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+    def after_sign_in_path_for(resource)
+      about_path
+    end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+    def after_sign_out_path_for(resource)
+      about_path
+    end
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
 end
